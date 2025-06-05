e97881210a9a73d3a11a65023d1b43335c6cac73
venkatasai-03 <141182370+venkatasai-03@users.noreply.github.com>
2025-06-05 22:15:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -432,5 +432,8 @@ def logout():
     flash('You have been logged out.', 'info')
     return redirect(url_for('home'))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
